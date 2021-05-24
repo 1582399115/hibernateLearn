20aeb79a6beaef8895a1c167f440d63b49c420ed
lby <lby@oo.com>
2021-05-24 17:41:31 +0800

    5.24

---
+++ HibernateDay01/src/com/xinlei/entity/User.java
@@ -0,0 +1,49 @@
+package com.xinlei.entity;
+
+public class User {
+    private long id;
+    private String userName;
+    private String UserPassword;
+    private int age;
+    private String gender;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return UserPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        UserPassword = userPassword;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
